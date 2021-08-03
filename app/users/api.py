bbb9920acff387091d9a0d8ee98620f4ac8604cb
dsgzds <anna7736@163.com>
2021-08-03 18:37:39 +0800

    ..

--- app/users/api.py
+++ app/users/api.py
@@ -1,13 +1,12 @@
-import json
+
 import datetime
 from flask import jsonify, request
 from app.users.model import Users, Extras, TeaInOrder, OrderModel, TeaModel, Tags
 from app.auth.auths import Auth
 from app import db
-from resource.order import order_schema
+from schemas.order import OrderSchema
 from .. import common
-
-
+order_schema = OrderSchema()
 def init_api(app):
     @app.route('/register', methods=['POST', 'GET'])
     def register():
@@ -83,21 +82,59 @@ def init_api(app):
             data = Tags.to_json(list)
             return jsonify(common.trueReturn(data, "获取分类成功"))
 
-    @app.route('/order', methods=['POST', 'GET'])
-    def post():
+
+    @app.route('/history_order', methods=['POST', 'GET'])
+    def HistoryOrder():
         if request.method == 'POST':
-            '''
+            user_id = request.form.get('user_id')
+            print(user_id)
+            list = OrderModel.query.filter(OrderModel.user_id == user_id).all()
+            data = OrderModel.to_json(list)
+
+            return jsonify(common.trueReturn(data, "获取历史订单成功"))
+
+    @app.route('/pay', methods=['POST', 'GET'])
+    def pay():
+        user_id = request.form.get('user_id')
+        user = Users.query.filter(Users.id == user_id).first()
+        order_id = request.form.get('order_id')
+        order = OrderModel.query.filter(OrderModel.id == order_id).first()
+        if user.balance >= order.price:
+            user.balance -= order.price
+        else:
+            return jsonify(common.falseReturn('', "余额不足"))
+
+        user.update()
+        order.change_status('payed')
+        return jsonify(common.trueReturn('', "支付成功"))
+
+    @app.route('/topup', methods=['POST', 'GET'])
+    def topup():
+        user_id = request.form.get('user_id')
+        user = Users.query.filter(Users.id == user_id).first()
+        num = request.form.get('num')
+        user.balance += int(num)
+        user.update()
+        return jsonify(common.trueReturn('', "充值成功"))
+
+    @app.route('/order', methods=['POST', 'GET'])
+    def Order():
+        if request.method == 'POST':
             data = request.get_json()
             Tea = []
+            time = datetime.datetime.now()
             ordered_list = data['Tea']  # list of dictionaries
-    
+            price = 0
+            user_id = data['user_id']
             for ordered_item in data['Tea']:
-                name = ordered_item['name']
+                TeaInOrderId = ordered_item['TeaInOrderId']
                 shop = ordered_item['shop']
-                price = db.session.query(TeaModel.price).fliter(TeaModel.name == name).all()
-                extras = ordered_item['extra'].split('+')
+
+                name = TeaModel.query.filter(TeaModel.id == TeaInOrderId).first().name
+                price += sum(db.session.query(TeaModel.price).filter(TeaModel.id == TeaInOrderId).first())
+                extras = ordered_item['extras'].split('+')
                 for extra in extras:
-                    price += db.session.query(Extras.price).fliter(Extras.name == extra).all()
+                    price += sum(db.session.query(Extras.price).filter(Extras.name == extra).first())
                 res = TeaModel.find_by_name(name)
                 if not res:
                     return {"msg": "Tea not present {}".format(name)}, 404
@@ -106,44 +143,22 @@ def init_api(app):
             # user = json.loads(init_api.get())
             # username = user['name']
             # phonenum = user['phonenum']
-            '''
-            Tea = request.form.get('name')
-            print(Tea)
-            Tea_info = TeaModel.query.filter(TeaModel.name == Tea).first()
-            Tea_id = Tea_info.id
-            price = Tea_info.price
-            extras = request.form.get('extras')
-            extras = extras.split('+')
-            shop = request.form.get('shop')
-            time = datetime.datetime.now()
-            for extra in extras:
-                extra_price = db.session.query(Extras.price).filter(Extras.name == extra).first()
-            
-                price += extra_price
+
+            # Tea = request.form.get('name')
             order = OrderModel(
-                TeaInOrderId=Tea_id,
+                TeaInOrderId=TeaInOrderId,
                 status="pending",
                 order_time=time,
-                # username=username,
-                # phonenum=phonenum,
+                user_id=user_id,
                 price=price,
                 shop=shop
             )
             order.save_to_db()  # save orders to database
+            import time
+            # time.sleep(600)
+            # order.request_with_stripe()  # send the order details to stripe
 
-            order.request_with_stripe()  # send the order details to stripe
-            print("Payment Done")
             order.change_status("success")
 
             return order_schema.dump(order)
 
-def GetUserInfo():
-    result = Auth.identify(Auth, request)
-    if result['status'] and result['data']:
-        user = Users.get(Users, result['data'])
-        returnUser = {
-            'id': user.id,
-            'username': user.username,
-            'phonenum': user.phonenum
-        }
-    return returnUser
--- app/users/model.py
+++ app/users/model.py
@@ -1,20 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.security import generate_password_hash, check_password_hash
-
+# from Models.order import OrderModel
 from app import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(250),  unique=True, nullable=False)
+    phonenum = db.Column(db.String(250),  unique=True, nullable=False)
     username = db.Column(db.String(250),  unique=True, nullable=False)
     password = db.Column(db.String(250))
+    balance = db.Column(db.Integer)
     login_time = db.Column(db.Integer)
+    order_id = db.relationship("OrderModel", backref="user_info")  # 用户下的订单
 
-    def __init__(self, username, password, email):
+    def __init__(self, username, password, phonenum):
         self.username = username
         self.password = password
-        self.email = email
+        self.phonenum = phonenum
+        self.balance = 100
 
     def __str__(self):
         return "Users(id='%s')" % self.id
@@ -47,3 +50,270 @@ def session_commit():
         db.session.rollback()
         reason = str(e)
         return reason
+
+
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+CURRENCY="INR"
+
+class BaseModel:
+    """模型基类，为每个模型补充创建时间与更新时间"""
+
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 记录的更新时间
+
+class TeaInOrder(db.Model):
+    __tablename__ = "tea_in_order"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tea_id = db.Column("tea_id", db.Integer, db.ForeignKey("tea.id"), nullable=True)
+    order_id = db.Column("order_id", db.String(100), db.ForeignKey("orders.id"), nullable=True)
+    price = db.Column(db.Integer, nullable=True)
+    state = db.Column(db.String(100), nullable=False)
+    ice = db.Column(db.String(100), nullable=False)
+    sweetness = db.Column(db.String(100), nullable=False)
+    lemon = db.Column(db.String(100), nullable=False)
+    tea_dreg = db.Column(db.String(100), nullable=False)  # 茶底
+    makeway = db.Column(db.String(100), nullable=False)  # 做法
+    taste = db.Column(db.String(100), nullable=False)  # 口味
+    # order = db.relationship("OrderModel", back_populates="tea")
+
+    def dobule_to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = str(getattr(self, key))
+            else:
+                result[key] = getattr(self, key)
+        return result
+
+        # 配合todict一起使用
+
+    def to_json(all_vendors):
+        v = [ven.dobule_to_dict() for ven in all_vendors]
+        return v
+
+
+class OrderModel(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    status = db.Column(db.String(15), nullable=False)
+    order_time = db.Column(db.String(100), nullable=True)
+    price = db.Column(db.Integer, nullable=True)
+    TeaInOrderId = db.Column(db.Integer, db.ForeignKey('tea.id'), nullable=True)
+    # TeaInOrder = db.relationship("TeaInOrder", back_populates="order_id", foreign_keys=[TeaInOrderId])
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    shop = db.Column(db.String(100), nullable=True)
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @property
+    def description(self):
+        counts = [f'{data.quantity}x {data.item.name}' for data in self.items]
+        return ",".join(counts)
+
+    @property
+    def amount(self):
+        total = int(sum([item_data.item.price * item_data.quantity for item_data in self.items]) * 100)
+        return total
+
+    def change_status(self, new_status):
+        self.status = new_status
+        self.save_to_db()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def status(self):
+        tmp_status = self.status
+        if self.status == 1:
+            tmp_status = self.express_status
+            if self.express_status == 1 and self.comment_status == 0:
+                tmp_status = -5
+            if self.express_status == 1 and self.comment_status == 1:
+                tmp_status = 1
+        return tmp_status
+
+    def dobule_to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = str(getattr(self, key))
+            else:
+                result[key] = getattr(self, key)
+        return result
+
+    # 配合todict一起使用
+    def to_json(all_vendors):
+        v = [ven.dobule_to_dict() for ven in all_vendors]
+        return v
+
+    '''
+    def request_with_stripe(self) -> stripe.Charge:
+        token = stripe.Token.create(
+            card={
+                "number": "4242424242424242",
+                "exp_month": 9,
+                "exp_year": 2021,
+                "cvc": "314",
+            }, )
+
+        print("The token id is ", token["id"])
+        print("The amount is ", self.amount)
+
+        return stripe.Charge.create(
+            amount=self.amount,
+            currency=CURRENCY,
+            description=self.description,
+            source=token["id"],
+            shipping={
+                'name': "John",
+                'address': {
+                    'line1': '510 Townsend St',
+                    'postal_code': '98140',
+                    'city': 'Kolkata',
+                    'state': 'WB',
+                    'country': 'India',
+                }
+            })
+    '''
+
+from app import db
+from typing import Dict, List, Union
+from flask_sqlalchemy import SQLAlchemy
+
+ItemJson = Dict[str, Union[int, str, float]]
+
+
+class TeaModel(db.Model):
+    __tablename__ = "tea"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),  nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    tag = db.Column(db.String(50), nullable=False)
+    remarks = db.Column(db.String(50), nullable=True)
+    images = db.Column(db.String(100), nullable=False)
+    db.ForeignKey('Tags.name')
+
+
+
+    # store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
+    # store = db.relationship("StoreModel")
+
+    def __init__(self, name, tag, price, description, remarks):
+        self.name = name
+        self.tag = tag
+        self.price = price
+        self.description = description
+        self.remarks = remarks
+
+    @classmethod
+    def find_by_name(cls, name: str):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, id: int):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls) -> List:
+        return cls.query.all()
+
+    @classmethod
+    def find_id(cls, name: str):
+        obj = cls.query.filter_by(name=name).first()
+        return obj.id
+
+    @classmethod
+    def get_info(cls, name: str):
+        info = cls.query.fliter(name == name).first()
+        return info
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+
+    def dobule_to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = str(getattr(self, key))
+            else:
+                result[key] = getattr(self, key)
+        return result
+
+    # 配合todict一起使用
+    def to_json(all_vendors):
+        v = [ven.dobule_to_dict() for ven in all_vendors]
+        return v
+
+class Tags(db.Model):
+    __tablename__ = "Tags"
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),  nullable=False)
+    db.relationship("TeaModel", backref='subcat')
+
+    def __init__(self, name):
+        self.name = name
+
+    def dobule_to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = str(getattr(self, key))
+            else:
+                result[key] = getattr(self, key)
+        return result
+
+    # 配合todict一起使用
+    def to_json(all_vendors):
+        v = [ven.dobule_to_dict() for ven in all_vendors]
+        return v
+
+class Extras(db.Model):
+    __tablename__ = "Extras"
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),  nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def dobule_to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = str(getattr(self, key))
+            else:
+                result[key] = getattr(self, key)
+        return result
+
+    # 配合todict一起使用
+    def to_json(all_vendors):
+        v = [ven.dobule_to_dict() for ven in all_vendors]
+        return v
+
+
+
